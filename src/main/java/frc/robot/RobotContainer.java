dea91e4a79578db6e069081cdef8776711f8f113
jcorr <jarrett@corr.org>
2025-12-01 20:10:39 -0800

    Added RobotContainer and other boilerplate.

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import java.util.Optional;
+import org.ironmaple.simulation.SimulatedArena;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+    private final SendableChooser<Command> autoChooser;
+
+    // The robot's subsystems and commands are defined here...
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        switch (Constants.currentMode) {
+            case REAL:
+
+                break;
+
+            case SIM:
+
+                break;
+            default:
+                break;
+        }
+
+        autoChooser = AutoBuilder.buildAutoChooser();
+        //NAMED COMMANDS IN SWERVE
+        SmartDashboard.putData("Autonomous Path", autoChooser);
+        // VisionSubsystem.getInstance();
+        ButtonConfig buttons = new ButtonConfig();
+        buttons.initTeleop();
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return autoChooser.getSelected();
+    }
+}
